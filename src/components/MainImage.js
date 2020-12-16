4e5766790545ec2ff95c84e7318aaca902751925
Caroline Leeper <caroline.leeper@gmail.com>
2020-12-16 20:51:46 +1100

    add museo font

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,5 +1,7 @@
+import style from "./MainImage.module.css";
+
 const MainImage = (props) => {
-  return <img src={props.mainImage} alt="" />;
+  return <img src={props.mainImage} alt="" className={style.mainImage} />;
 };
 
 export default MainImage;
